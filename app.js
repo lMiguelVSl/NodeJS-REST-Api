2c8230e2dd3a33a7658ed12ef00c03596bc4c415
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-09 19:45:41 -0500

    Allowing Cors

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.next();
+})
+
 app.use('/feed', feedRoutes.router);
 
 app.use('/', (req, res) => res.send('Welcome Page'));
--- routes/feed.js
+++ routes/feed.js
@@ -7,7 +7,7 @@ const router = express.Router();
 // GET /feed/posts
 router.get('/posts', feedController.getPosts);
 //POST /feed/posts
-router.post('/posts', feedController.createPost);
+router.post('/post', feedController.createPost);
 
 module.exports = {
     router: router
