454232f9bf9d9c9213463ba1b70a420883f09c75
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-15 19:32:09 -0500

    middleware fix

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const feedRoutes = require('./routes/feed');
 
+const port = 8000;
 const app = express();
 
 app.use(bodyParser.json());
@@ -11,11 +12,12 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.next();
+    next();
 })
 
 app.use('/feed', feedRoutes.router);
 
 app.use('/', (req, res) => res.send('Welcome Page'));
 
-app.listen(8000);
+app.listen(port);
+console.log('Started in port:', port);
